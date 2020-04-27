9203e879d8cc29c4741d20fdfec6f0aef75ae229
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-27 16:44:37 +0600

    Add files via upload

---
+++ Sumofnumber.java
@@ -0,0 +1,21 @@
+
+package sumofnumber;
+
+import java.util.Scanner;
+
+public class Sumofnumber {
+
+
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    int m,n,sum;
+    System.out.println("eneter first number");
+    m=input.nextInt();
+    System.out.println("eneter second number");
+    n=input.nextInt();
+    sum=m+n;
+    System.out.println("the sum is "+sum);
+  
+    }
+    
+}
